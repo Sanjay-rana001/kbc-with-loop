6a7c5ca5ab7d0a48e5b307c2bd8e4ef28f833854
sanjay-rana001 <sanjay00002023@gmail.com>
2023-02-14 22:03:14 +0530

    commit

---
+++ faltu.py
@@ -0,0 +1,9 @@
+sanjay=[["ram", "shyam","mohan","suresh"],["ram", "shyam","mohan","suresh"],["ram", "shyam","mohan","suresh"]]
+ram="kajal", "mohit","raja","katil"
+levels=[100,200,300,400,500,600]
+for i in range(0,len(sanjay)):
+    sanjays=sanjay[i]
+    print(f"sanjays for {levels[i]}")
+    print(ram[i])
+
+
